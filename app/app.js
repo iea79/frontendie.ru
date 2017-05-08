0e21de4e50a28ba119b3ede70945e9a013cdd3bd
Evgeny Ivanov <busforward@gmail.com>
2017-05-08 16:36:28 +0500

    fix

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 //@prepros-append ../js/jquery.js
+//@prepros-append ../js/jquery-ui.min.js
 //@prepros-append ../js/jquery.circliful.min.js
 //@prepros-append ../js/jquery.viewportchecker.js
 //@prepros-append ../js/slick.min.js
@@ -6,5 +7,7 @@
 //@prepros-append ../js/modal.js
 //@prepros-append ../js/tab.js
 //@prepros-append ../js/portfolio.js
-//@prepros-append ../js/function.js
 //@prepros-append ../js/validate.js
+//@prepros-append ../js/pollyfill.js
+//@prepros-append ../js/jquery.textanimation.js
+//@prepros-append ../js/function.js
--- js/function.js
+++ js/function.js
@@ -63,6 +63,42 @@ $(document).ready(function() {
 	    });
 	}
 
+	// First screen scroller 
+    var isAnimating  = false;
+	var lastScrollTop = 0;
+
+	$(window).scroll(function(event){
+		var st = $(this).scrollTop();
+        // e.preventDefault();
+        // e.stopPropagation();
+
+	    var headerTop = $('#first_screen').height();
+	    	nextSection = $('#about').offset().top;
+
+		if( isAnimating ) {
+		   return false;
+		}
+
+		if (st > lastScrollTop){
+		   // downscroll code
+		    isAnimating  = true;
+		    if ($(window).scrollTop() < headerTop) {	    	
+		        $('html, body').animate({ scrollTop: nextSection }, 500, function(){
+		            isAnimating  = false;
+		        });
+		    } else {
+				isAnimating  = false;
+		    }
+
+		} else {
+			isAnimating  = false;
+		}
+
+		lastScrollTop = st;
+
+	});
+
+
 	// Portfolio filter
 	$('.portfolio_grid .grid__wrapper').addClass('hidden');
 
@@ -109,7 +145,7 @@ $(document).ready(function() {
 
 			$(".about_text span").typed({
 		        strings: ["^1500 Меня зовут Евгений, ^50 я профессиональный HTML-верстальщик. <br/>^50 Занимаюсь вёрсткой сайтов с 2010 года, <br/>^50  для меня это не просто хобби, а основной заработок и поэтому к работе отношусь серьезно и с большой ответственностью."],
-		        typeSpeed: 1,
+		        typeSpeed: -100,
 		        preStringTyped: function() {
 		        	audioPlay();
 		        },
@@ -123,10 +159,10 @@ $(document).ready(function() {
 
 	$('.skills_grid').viewportChecker({
 		callbackFunction: function () {
-			setTimeout(function() {
-				$('#about .section_title').addClass('open');
+			// setTimeout(function() {
+				// $('#about .section_title').addClass('open');
 
-				$('.skills_grid').addClass('open');
+				// $('.skills_grid').addClass('open');
 				// Circleful.js
 				$('#skill_html').circliful({
 					percent: 95,
@@ -143,7 +179,7 @@ $(document).ready(function() {
 				$('#skill_js').circliful({
 					percent: 60,
 				});
-			}, 11000);
+			// }, 6000);
 		}
 	});
 
@@ -169,7 +205,7 @@ $(document).ready(function() {
 		dots: true,
 		arrows: false,
 		slidesToShow: 5,
-		slidesToScroll: 1,
+		slidesToScroll: 5,
 		autoplay: true,
 		autoplaySpeed: 2000,
 		responsive: [
@@ -177,7 +213,7 @@ $(document).ready(function() {
 		      breakpoint: 1024,
 		      settings: {
 		        slidesToShow: 4,
-		        slidesToScroll: 1
+		        slidesToScroll: 4
 		      }
 		    },
 		    {
@@ -219,6 +255,44 @@ $(document).ready(function() {
 	}
 
 	filleUpload();
+
+	if ($(window).width() > 1024) {	
+	    // Init pollifill
+	    $('.bg_canvas').constellation({
+	        star: {
+	            width: 2,
+	            color: 'rgba(255, 255, 255, 0.4)'
+	        },
+	        line: {
+	            color: 'rgba(255, 255, 255, 0.2)'
+	        },
+	        radius: 300
+	    });
+
+	    $('.technology .bg_canvas').constellation({
+	        star: {
+	            width: 2,
+	            color: 'rgba(32, 163, 255, 0.3)'
+	        },
+	        line: {
+	            color: 'rgba(32, 163, 255, 0.1)'
+	        },
+	        radius: 100
+	    })
+
+	    // Title animation
+	    $(".first_screen_title span").textAnimation({
+	        mode: "highlight",
+	        baseColor: "#ffffff",
+	        highlightColor: "rgb(26,131,205, 0.3)",
+	        delay: 35,
+	        interval: 7000,
+	        duration: 50
+	    });  	
+
+	} else {
+		$('.bg_canvas').hide();
+	}
 
 });
 
